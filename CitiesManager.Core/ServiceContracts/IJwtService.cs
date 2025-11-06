771b06ec49d6ac8e6d6b6a1fcdd518b08dc68f7c
Amir Mahdi Temyouri <amirmahditeymoori123@gmail.com>
2025-11-06 23:28:42 +0330

    first steps to jwt implementation

---
+++ CitiesManager.Core/ServiceContracts/IJwtService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitiesManager.Core.DTOs;
+using CitiesManager.Core.Identity;
+
+namespace CitiesManager.Core.ServiceContracts
+{
+    public interface IJwtService
+    {
+        AuthenticationResponse CreateJwtToken(User user);
+    }
+}
---
+++ CitiesManager.Core/Services/JwtService.cs
@@ -0,0 +1,52 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using CitiesManager.Core.DTOs;
+using CitiesManager.Core.Identity;
+using CitiesManager.Core.ServiceContracts;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using JwtRegisteredClaimNames = Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames;
+
+namespace CitiesManager.Core.Services
+{
+    public class JwtService : IJwtService
+    {
+        private readonly IConfiguration _configuration;
+        public JwtService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public AuthenticationResponse CreateJwtToken(User user)
+        {
+            var issuer = _configuration["jwt:Issuer"]; // who makes the token?
+            var audience = _configuration["jwt:Audience"]; // who uses the token?
+            var expirationDate= DateTime.UtcNow.AddMinutes(Convert.ToDouble(_configuration["jwt:EXPIRATION_MINUTES"])); // when token expires?
+            var key = _configuration["jwt:key"]; // secret key used for encryption (has to be env variables but we are in development )
+
+            var claims = new Claim[]
+            {
+                // the subject (sub), issued at (iat), jti (jwt unique id) are recommended claims and not optional.
+
+                new (JwtRegisteredClaimNames.Sub, user.Id.ToString()), // subject (user id)
+                new (JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()), // issued at (date and time of the token generated)
+                new (JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()), // JWT ID (unique identifier for the token)
+                new (ClaimTypes.NameIdentifier, user.Email) // unique name identifier of the user (Email) (optional)
+            };
+
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+
+            var signInCreds = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var tokenGenerator = new JwtSecurityToken(issuer: issuer, 
+                audience: audience,
+                claims: claims, 
+                expires: expirationDate, 
+                signingCredentials: signInCreds);
+
+            var token = new JwtSecurityTokenHandler().WriteToken(tokenGenerator);
+
+            return new AuthenticationResponse() {Email = user.Email, ExpireDate = expirationDate, Token = token};
+        }
+    }
+}
